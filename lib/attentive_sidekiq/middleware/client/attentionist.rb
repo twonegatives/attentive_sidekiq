161958257789a44bce26b200f2a372c60e934c9a
twonegatives <whitewhiteheaven@gmail.com>
2016-11-04 01:24:31 +0300

    removed commented lines of code

--- lib/attentive_sidekiq/middleware/client/attentionist.rb
+++ lib/attentive_sidekiq/middleware/client/attentionist.rb
@@ -2,23 +2,11 @@ module AttentiveSidekiq
   module Middleware
     module Client
       class Attentionist
-
         def call(worker_class, item, queue, redis_pool = nil)
+          # TODO: we could backup job info here aswell
+          # this would lead us to the need of more complex records filtering
           yield
         end
-
-        #def call(worker_class, item, queue, redis_pool = nil)
-        #  yield
-        #  if rerun?(item["jid"])
-        #    mark_as_not_lost(item["jid"])
-        #  else
-        #  add_to_observed_list(item)
-        #  end
-        #end
-
-        #def rerun?(jid)
-        #  Sidekiq.redis{|conn| conn.hexists(AttentiveSidekiq::Middleware::REDIS_KEY, jid) } == 1
-        #end
       end
     end
   end
