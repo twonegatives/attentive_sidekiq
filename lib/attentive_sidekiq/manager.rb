212b3c0fbed0a036b1a29b5baff516fbe71d2007
Hussein El Motayam <HusseinElMotayam@gmail.com>
2018-01-15 03:36:03 +0200

    Extra disappeared check to avoid false-positives
    This should fix https://github.com/twonegatives/attentive_sidekiq/issues/7

--- lib/attentive_sidekiq/manager.rb
+++ lib/attentive_sidekiq/manager.rb
@@ -18,6 +18,15 @@ module AttentiveSidekiq
       suspicious  = AttentiveSidekiq::Suspicious.jobs
       active_ids  = AttentiveSidekiq::Active.job_ids
       those_lost  = suspicious.delete_if{|i| active_ids.include?(i["jid"])}
+      
+      # Sidekiq might have been too fast finishing up a job that appeared in the suspicious list
+      # but didn't make it to the active list, so that's a false-positive.
+      # We need to get the new suspicious list again, and remove any lost jobs that are no longer there.
+      # Those jobs that appeared in the first suspicious list, but not the second one were simply finished
+      # quickly by Sidekiq before showing up as active by a worker.
+      suspicious  = AttentiveSidekiq::Suspicious.jobs
+      those_lost.delete_if{|i| !suspicious.include?(i["jid"])}
+      
       those_lost.each do |job|
         Disappeared.add(job)
         Suspicious.remove(job['jid'])
